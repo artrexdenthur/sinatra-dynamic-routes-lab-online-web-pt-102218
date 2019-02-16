c6d31da37840d654e24ef94836c0b102b00452df
Paul Ashour <paul.ashour@gmail.com>
2019-02-16 17:34:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
     # params[:number].to_i.times do
     #   "#{params[:phrase]}"
     # end
-    status 200
+    # status 200
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
